c6f7cb7a599e43c8c5357eae077fe8cba3ce185d
Nova-Striker <64089730+Nova-Striker@users.noreply.github.com>
2020-08-06 00:06:03 +0530

    Update Database.py

--- Database.py
+++ Database.py
@@ -1,26 +1,18 @@
 import sqlite3
 
-
 class make_db:
     def __init__(self, dbname):
         self.dbname = dbname
         self.mydb = sqlite3.connect(self.dbname + '.db')
-
         self.cursor = self.mydb.cursor()
-
-        self.cursor.execute("""CREATE TABLE IF NOT EXISTS DATA(
-										RollNo INTEGER,
-										StudentName TEXT,
-										Marks INT )
-										""")
+        self.cursor.execute("""CREATE TABLE IF NOT EXISTS DATA(RollNo INTEGER,StudentName TEXT,Marks INT )""")
 
     def store_values(self, RollNo, StudentName, Marks):
         self.cursor.execute("INSERT INTO DATA VALUES(:Rollno , :StudentName , :Marks)", {
                             'Rollno': RollNo, 'StudentName': StudentName, 'Marks': Marks})
         self.mydb.commit()
 
-
-class get_response(make_db):
+	class get_response(make_db):
     def __init__(self, Database, query):
         self.query = query
         self.Database = Database
