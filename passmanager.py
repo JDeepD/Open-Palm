b4e327376f360e50361080fecfb4dbde00829b98
JayD <jaydeepjd.1125@gmail.com>
2020-09-05 15:15:13 +0530

    Password Manager Code

---
+++ passmanager.py
@@ -0,0 +1,20 @@
+# This is very very bad encrytion and is only implemented for simplicity of the Project.
+# Please donot store the same password as your other accounts like Gmail,Facebook etc. Use this at your own risk
+import csv
+def storepass(user,passwd):
+    with open("data.csv",'a+') as fil :
+        writer = csv.writer(fil) 
+        writer.writerow([user,passwd])
+def cipherpass(passwd):
+    tmp = ""    
+    for i in passwd:
+       tmp += chr(ord(i)*2) 
+    return(tmp)
+
+def decipherpass(encr):
+    tmp = ""
+    for i in encr:
+        tmp += chr(int(ord(i)/2))
+    return(tmp)
+
+
