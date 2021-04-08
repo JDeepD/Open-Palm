02abea9b150caf5086f1020221bcde023b51a730
Jaydeep Das <jaydeepjd.1125@gmail.com>
2021-04-08 21:21:44 +0530

    Dist (#18)
    * (Added build: v0.1)
    
    * Renamed a folder
    
    * Added executable for windows
    
    * (Bug Fix)
    
    Bug: Unable to access database using compiled version
    Reason :The compiled version did not create `creds/admins.csv` on first
    launch

---
+++ src/output/test_it.py
@@ -0,0 +1,9 @@
+#It is highly recommended that you check your code first in IDLE first for syntax errors and then Test it here.
+
+#If your code has syntax errors ,Open-Palm will freeze. You have to restart it in that case 
+
+def check_even(n):
+	if n%2 == 0:
+		return True
+	else:
+		return False
